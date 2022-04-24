ab131846f5bfd5478c0e170393e6d16536f18e53
Andy Wilson <31218527+The-Code-Monkey@users.noreply.github.com>
2022-04-24 13:27:31 +0100

    Update createProgressEstimator.ts

--- packages/tcm-cli/src/createProgressEstimator.ts
+++ packages/tcm-cli/src/createProgressEstimator.ts
@@ -1,8 +1,7 @@
 import fs from 'fs-extra';
+import progressEstimator from 'progress-estimator';
 
 import { paths } from './constants.js';
-
-const progressEstimator = require('progress-estimator');
 
 export async function createProgressEstimator() {
   await fs.ensureDir(paths.progressEstimatorCache);
