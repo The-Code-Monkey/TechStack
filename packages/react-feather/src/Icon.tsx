57071057acbd98e578f7c23a657c4b4ba041822e
Andy Wilson <31218527+The-Code-Monkey@users.noreply.github.com>
2022-04-28 21:34:35 +0100

    Update Icon.tsx

--- packages/react-feather/src/Icon.tsx
+++ packages/react-feather/src/Icon.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, Suspense, memo } from 'react';
+import React, { Suspense, memo } from 'react';
 import { default as HelpCircle } from './help-circle';
 
 import { IconProps } from './types';
@@ -7,7 +7,7 @@ export interface Props extends IconProps {
   name: keyof typeof IconTypes;
 }
 
-const getIcon = async (name) => await import(`./icons/${name}`)
+const getIcon = async (name) => await import(`./icons/${name}`);
 
 const Icon = async ({ name, ...rest }: Props) => {
   const Element = await getIcon(name);
