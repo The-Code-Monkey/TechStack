116471a4a4a45a052302d95c84b826bbfc0b3583
Andy Wilson <31218527+The-Code-Monkey@users.noreply.github.com>
2022-04-28 21:15:50 +0100

    Create Icon.tsx

---
+++ packages/react-feather/src/Icon.tsx
@@ -0,0 +1,22 @@
+import React, { useContext, Suspense, memo } from 'react';
+import { default as HelpCircle } from './help-circle';
+
+import { IconProps } from './types';
+
+export interface Props extends IconProps {
+  name: keyof typeof IconTypes;
+}
+
+const getIcon = async (name) => await import(`./icons/${name}`)
+
+const Icon = async ({ name, ...rest }: Props) => {
+  const Element = await getIcon(name);
+
+  return (
+    <Suspense fallback={<HelpCircle />}>
+      <Element />
+    </Suspense>
+  );
+};
+
+export default memo(Icon);
