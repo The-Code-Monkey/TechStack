8c25587e4df46942ad1cff44daf8816371f6dc0e
codefactor-io <support@codefactor.io>
2022-04-29 08:32:37 +0000

    [CodeFactor] Apply fixes

--- packages/react-feather/src/Icon.tsx
+++ packages/react-feather/src/Icon.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+
 import { default as HelpCircle } from './icons/help-circle';
 import { IconProps } from './types';
 
--- packages/react-feather/src/icons/help-circle.tsx
+++ packages/react-feather/src/icons/help-circle.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+
 import { IconProps } from '../types';
 
 const HelpCircle = (
