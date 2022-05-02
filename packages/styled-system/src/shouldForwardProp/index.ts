2252b43fa937accb2703c6c3b6315a79e1a78498
Andy Wilson <31218527+The-Code-Monkey@users.noreply.github.com>
2022-05-02 07:25:52 +0100

    fix: update and add some changes

--- packages/styled-system/src/shouldForwardProp/index.ts
+++ packages/styled-system/src/shouldForwardProp/index.ts
@@ -1,5 +1,5 @@
 import isPropValid from '@emotion/is-prop-valid';
-import memoize from '@emotion/memoize';
+import memoize from "memoize-one";
 
 import { compose } from '../core';
 import { animation } from '../parsers/animation';
