30b2a8fcc02b5298f92024478c3652f02e6e15d2
Andy Wilson <31218527+The-Code-Monkey@users.noreply.github.com>
2022-05-06 09:47:01 +0100

    fix: add publish config

--- packages/components/.storybook/ThemedWrapper.tsx
+++ packages/components/.storybook/ThemedWrapper.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import addons from "@storybook/addons";
 import { ThemeProvider } from "styled-components";
 
