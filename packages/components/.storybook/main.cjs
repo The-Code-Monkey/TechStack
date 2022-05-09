24a00d7abd2b7b8556d7cbecebde863488bf1688
Andy Wilson <31218527+The-Code-Monkey@users.noreply.github.com>
2022-05-09 10:52:09 +0100

    fix: issue with styled components and storybook on build

--- packages/components/.storybook/main.cjs
+++ packages/components/.storybook/main.cjs
@@ -1,8 +1,19 @@
+const webpack = require("webpack");
+
 module.exports = {
   stories: ['../src/@(atoms|molecules|organisms|primatives)/**/story.@(ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
   typescript: {
     check: true, // type-check stories during Storybook build
+  },
+  webpackFinal: (config) => {
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        SC_DISABLE_SPEEDY: true
+      })
+    );
+
+    return config;
   }
 };
