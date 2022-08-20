d553e9c12f44143011d94b17d645ba4e31767c2a
Andy Wilson <31218527+The-Code-Monkey@users.noreply.github.com>
2022-08-20 17:58:21 +0100

    Delete docs directory

--- docs/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import '../styles/globals.css'
-import 'nextra-theme-docs/style.css'
-import type { AppProps } from 'next/app'
-
-const Main = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
-}
-
-export default Main;
