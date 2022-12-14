0118b37e592f7bb7fb4d476ffc7c345c475d64ce
Andy Wilson <31218527+The-Code-Monkey@users.noreply.github.com>
2022-12-14 16:00:46 +0000

    fix: remove styled div in theme provider

--- packages/components/src/utils/theme-provider/theme-provider.tsx
+++ packages/components/src/utils/theme-provider/theme-provider.tsx
@@ -3,23 +3,20 @@ import { ThemeProvider as StyledThemeProvider } from 'styled-components';
 
 import createTheme from './create-theme';
 import { ThemeModeEnum } from './enum';
-import { StyledDiv } from './styled';
 import { ITheme } from './types';
 
 interface Props<V> {
   children: ReactNode;
   theme?: ITheme<V>;
   mode?: ThemeModeEnum;
-  direction?: 'row' | 'column';
 }
 
 export const ThemeProvider = <V extends object>({
   children,
   theme,
   mode,
-  direction = 'column',
 }: Props<V>) => (
   <StyledThemeProvider theme={createTheme(theme, mode)}>
-    <StyledDiv direction={direction}>{children}</StyledDiv>
+    {children}
   </StyledThemeProvider>
 );
