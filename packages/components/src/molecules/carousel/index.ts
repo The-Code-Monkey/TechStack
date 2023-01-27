14d7fb4c92418e4cfcd619af925c76554471a045
Andy Wilson <31218527+The-Code-Monkey@users.noreply.github.com>
2023-01-27 15:49:37 +0000

    fix: add carousel to exports

--- packages/components/src/molecules/carousel/index.ts
+++ packages/components/src/molecules/carousel/index.ts
@@ -1,5 +1,5 @@
 import Carousel from './Carousel';
 import type { Props as CarouselProps } from './Carousel';
 
-export { CarouselProps };
+export { CarouselProps, Carousel };
 export default Carousel;
