17e5c0dc5b6ec0c8e7dd3336ffa9931aef05baa5
Andy Wilson <31218527+The-Code-Monkey@users.noreply.github.com>
2023-01-31 11:22:08 +0000

    fix: add type button as default on button

--- packages/components/src/atoms/button/Button.tsx
+++ packages/components/src/atoms/button/Button.tsx
@@ -32,6 +32,7 @@ const Button = ({
   iconPosition = 'left',
   testid,
   variant = 'default',
+  type = 'button',
   ...rest
 }: Props) => {
   const hasChildren = useMemo(
@@ -46,6 +47,7 @@ const Button = ({
       testid={`${testid || children}_button`}
       typography='button'
       variant={variant}
+      type={type}
       {...rest}
       {...iconOrientations[iconPosition]}
     >
