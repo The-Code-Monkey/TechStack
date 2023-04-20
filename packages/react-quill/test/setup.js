c7262f46780020a2b3c3357a324c4c8db718a22d
Andy Wilson <31218527+The-Code-Monkey@users.noreply.github.com>
2023-04-20 17:48:43 +0100

    add: add react-quill and up quill to v2

---
+++ packages/react-quill/test/setup.js
@@ -0,0 +1,22 @@
+/**
+ * Create mock DOM and browser globals
+ *
+ * See Enzyme docs:
+ * https://github.com/airbnb/enzyme/blob/master/docs/guides/jsdom.md
+ * https://github.com/airbnb/enzyme/blob/master/docs/api/mount.md
+ */
+
+require('jsdom-global')();
+require('./polyfills/MutationObserver.js')(global);
+require('./polyfills/getSelection.js')(global);
+const chai = require('chai');
+const chaiEnzyme = require('chai-enzyme');
+const Enzyme = require('enzyme');
+const EnzymeAdapter = require('enzyme-adapter-react-16');
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+// Setup Chai to use Enzyme
+chai.use(chaiEnzyme());
+
+global.expect = chai.expect;
---
+++ packages/react-quill/test/utils.js
@@ -0,0 +1,45 @@
+var { mount } = require('enzyme');
+var React = require('react');
+
+var ReactQuill = require('../dist/index');
+
+function ReactQuillNode(props, children) {
+  props = Object.assign(
+    {
+      modules: { toolbar: ['underline', 'bold', 'italic'] },
+      formats: ['underline', 'bold', 'italic'],
+    },
+    props
+  );
+
+  return React.createElement(ReactQuill, props, children);
+}
+
+function mountReactQuill(props, node) {
+  return mount(ReactQuillNode(props, node));
+}
+
+function getQuillInstance(wrapper) {
+  return wrapper.instance().getEditor();
+}
+
+function getQuillDOMNode(wrapper) {
+  return wrapper.getDOMNode().querySelector('.ql-editor');
+}
+
+function getQuillContentsAsHTML(wrapper) {
+  return getQuillDOMNode(wrapper).innerHTML;
+}
+
+function setQuillContentsFromHTML(wrapper, html) {
+  const editor = getQuillInstance(wrapper);
+  return editor.clipboard.dangerouslyPasteHTML(html);
+}
+
+module.exports = {
+  mountReactQuill,
+  getQuillInstance,
+  getQuillDOMNode,
+  getQuillContentsAsHTML,
+  setQuillContentsFromHTML,
+};
