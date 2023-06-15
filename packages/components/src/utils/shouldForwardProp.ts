c3fc83a002b4ba60ebb15d4cd010a6a462d023ce
Andy Wilson <31218527+The-Code-Monkey@users.noreply.github.com>
2023-06-15 21:42:06 +0100

    fix: linting

--- packages/components/src/utils/shouldForwardProp.ts
+++ packages/components/src/utils/shouldForwardProp.ts
@@ -1,13 +1,11 @@
 import isPropValid from '@emotion/is-prop-valid';
 import memo from '@techstack/memoize';
 
-const forwardPropHelper =
-  (styledProps: Array<string>) =>
-  (prop: string) => {
-    const regex = new RegExp(`^(${styledProps.join('|')})$`);
+const forwardPropHelper = (styledProps: Array<string>) => (prop: string) => {
+  const regex = new RegExp(`^(${styledProps.join('|')})$`);
 
-    if (prop === 'testid') return true;
-    return isPropValid(prop) && !regex.test(prop);
-  };
+  if (prop === 'testid') return true;
+  return isPropValid(prop) && !regex.test(prop);
+};
 
 export const shouldForwardProp = memo(forwardPropHelper);
