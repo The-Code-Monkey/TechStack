9ee110f173b7b5a8c847e6b0c7ef17d121bc8947
Andy Wilson <andy.wilson1234@gmail.com>
2023-08-12 18:16:23 +0100

    Update defaults.ts

--- packages/tcm-cli/src/defaults.ts
+++ packages/tcm-cli/src/defaults.ts
@@ -2,6 +2,7 @@ import { RollupOptions } from 'rollup';
 
 import { NormalizedTcmConfig } from './types';
 
+
 export const configDefaults: NormalizedTcmConfig = Object.freeze({
   rollup: (config: RollupOptions) => config,
 });
