5a99dd90366e5e9ef2d63e110bd50fa0290833a7
Andy Wilson <andy@builtbypixel.com>
2023-09-21 09:50:34 +0100

    update stuff

--- build.ts
+++ build.ts
@@ -1,3 +1,4 @@
+import { exec } from 'child_process';
 
 const buildTheme = Bun.spawn(["bun", "run", "build"], {
     cwd: "./packages/theme"
@@ -5,13 +6,24 @@ const buildTheme = Bun.spawn(["bun", "run", "build"], {
 
 await buildTheme.exited;
 
-Bun.spawn(["bun", "run", "build:tokens"], {
-    cwd: "./packages/components"
+Bun.spawn(["bun", "install"])
+
+const packages = ['memoize', 'react-textfit', 'react-feather', 'font-picker', 'react-lazy-named'];
+
+const promises: Array<Promise<any>> = packages.map((key) => Bun.build({
+    entrypoints: [`./packages/${key}/src/index.ts`],
+    outdir: `./packages/${key}/dist/esm`,
+    target: 'browser',
+    format: 'esm'
+}));
+
+await Promise.all(promises).then(values => console.log(values));
+
+exec('cd packages/components && bun run build:tokens')
+
+await Bun.build({
+    entrypoints: ["./packages/components/src/index.ts"],
+    outdir: "./packages/components/dist/esm",
+    target: "browser",
+    format: "esm"
 })
-//
-// await Bun.build({
-//     entrypoints: ["./packages/components/src/index.ts"],
-//     outdir: "./packages/components/dist/esm",
-//     target: "browser",
-//     format: "esm"
-// })
