f430b3f67f5260c21b8a32164cfa0611ceb67adb
Andy Wilson <andy@builtbypixel.com>
2023-09-21 09:35:36 +0100

    update stuff

--- build.ts
+++ build.ts
@@ -2,18 +2,16 @@
 const buildTheme = Bun.spawn(["bun", "run", "build"], {
     cwd: "./packages/theme"
 })
+
 await buildTheme.exited;
 
-const buildTokens = Bun.spawn(["bun", "run", "build:tokens"], {
+Bun.spawn(["bun", "run", "build:tokens"], {
     cwd: "./packages/components"
 })
-
-
-// exec("cd packages/components && bun run build:tokens")
-
-const build = await Bun.build({
-    entrypoints: ["./packages/components/src/index.ts"],
-    outdir: "./packages/components/dist/esm",
-    target: "browser",
-    format: "esm"
-})
+//
+// await Bun.build({
+//     entrypoints: ["./packages/components/src/index.ts"],
+//     outdir: "./packages/components/dist/esm",
+//     target: "browser",
+//     format: "esm"
+// })
