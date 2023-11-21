fb09549b41f16784c941b7f0289b9ff862f5d3fe
Andy Wilson <andy@builtbypixel.com>
2023-11-21 12:15:10 +0000

    fix: stash

--- packages/components/.storybook/main.ts
+++ packages/components/.storybook/main.ts
@@ -10,7 +10,7 @@ function getAbsolutePath(value: string): any {
   return dirname(require.resolve(join(value, "package.json")));
 }
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ['../src/@(atoms|molecules|organisms|primal)/**/story.@(ts|tsx)'],
   addons: [
     getAbsolutePath("@storybook/addon-links"),
     getAbsolutePath("@storybook/addon-essentials"),
