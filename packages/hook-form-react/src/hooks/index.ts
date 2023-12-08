19018e1c3ca97174f43e334a5a3c6fa5a616c6ad
Andy Wilson <andy.wilson1234@gmail.com>
2023-12-08 23:22:33 +0000

    Update index.ts

--- packages/hook-form-react/src/hooks/index.ts
+++ packages/hook-form-react/src/hooks/index.ts
@@ -1,11 +1,15 @@
 import useForm, { useFormReturnType, useFormProps } from './useForm';
 import useField, { useFieldReturnType, useFieldProps } from './useField';
+import useFields, { useFieldsReturnType, useFieldsProps } from './useFields';
 
 export {
   useForm,
   useField,
+  useFields,
   useFormProps,
   useFieldProps,
+  useFieldsProps,
   useFormReturnType,
   useFieldReturnType,
+  useFieldsReturnType,
 };
