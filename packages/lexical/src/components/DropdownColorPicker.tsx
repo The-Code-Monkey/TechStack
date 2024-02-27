090a594dc5d6805821cc1da7f97eb99056b66e04
Andy Wilson <andy@builtbypixel.com>
2024-02-27 07:43:44 +0000

    fix: lexical now works after being built

--- packages/lexical/src/components/DropdownColorPicker.tsx
+++ packages/lexical/src/components/DropdownColorPicker.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react';
+import { ReactNode } from 'react';
 
 import ColorPicker from './ColorPicker';
 import DropDown from './Dropdown';
--- packages/lexical/src/index.tsx
+++ packages/lexical/src/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { CodeHighlightNode, CodeNode } from '@lexical/code';
 import { $generateNodesFromDOM } from '@lexical/html';
 import { AutoLinkNode, LinkNode } from '@lexical/link';
