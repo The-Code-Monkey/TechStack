22cae402221f742afd9af2a32549506350328591
Andy Wilson <andy@builtbypixel.com>
2024-05-03 10:58:25 +0100

    fix: remove if statement

--- packages/lexical/src/plugins/TableActionMenuPlugin.tsx
+++ packages/lexical/src/plugins/TableActionMenuPlugin.tsx
@@ -69,9 +69,6 @@ function isTableSelectionRectangular(selection: TableSelection): boolean {
     const node = nodes[i];
     if ($isTableCellNode(node)) {
       const row = node.getParentOrThrow();
-      if ($isTableRowNode(row)) {
-        throw new Error('Expected CellNode to have a RowNode parent');
-      }
       if (currentRow !== row) {
         if (expectedColumns !== null && currentColumns !== expectedColumns) {
           return false;
